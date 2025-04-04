2ac706355e2b47abf5494f5ee8d268f7b949e6e8
Jay Joo <jayjoo0727@gmail.com>
2025-04-03 17:55:18 -0700

    jj - updated with actual github id

--- src/test/java/edu/ucsb/cs156/spring/hello/DeveloperTest.java
+++ src/test/java/edu/ucsb/cs156/spring/hello/DeveloperTest.java
@@ -25,6 +25,11 @@ public class DeveloperTest {
         assertEquals("Jay J.", Developer.getName());
     }
 
+    @Test
+    public void getGithubId_returns_correct_githubId() {
+        assertEquals("tizerk", Developer.getGithubId());
+    }
+
     // TODO: Add additional tests as needed to get to 100% jacoco line coverage, and
     // 100% mutation coverage (all mutants timed out or killed)
 
